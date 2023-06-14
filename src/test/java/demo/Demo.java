a4208d973d45dc07febc5b2806fa1d257e9505f7
neelakantareddy <neelakantareddy@DESKTOP-V1BIQ48>
2023-06-14 20:38:11 +0530

    TC-001

--- src/test/java/demo/Demo.java
+++ src/test/java/demo/Demo.java
@@ -4,6 +4,8 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("T.M.S.R");
+		System.out.println("JYOTHI");
+		System.out.println("test script TC-001");
 	}
 
 }
